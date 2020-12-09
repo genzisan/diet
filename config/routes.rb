fa1f209a3e321f6f3e712cfd800cf3f89a7f09a4
genizi <73005681+genizi@users.noreply.github.com>
2020-12-09 10:00:12 +0900

    　コントローラー、ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
+  resources :pages do 
+      collection do
+      get 'second'
+    end
+  end
+  
+
 end
