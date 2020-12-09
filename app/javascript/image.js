ead3ab19fde949b340aaac4490b266962b5eade8
genizi <73005681+genizi@users.noreply.github.com>
2020-12-09 10:00:46 +0900

    javascriptファイル編集、読み込み記述

---
+++ app/javascript/image.js
@@ -0,0 +1,31 @@
+window.addEventListener('load',() => {
+  const image = document.querySelectorAll(".image-one");
+  document.addEventListener("scroll",() => {
+    for (let i =0; i < image.length; i++) {
+      const imageTop = image[i].getBoundingClientRect().top + image[i].clientHeight * .6
+      if (window.innerHeight > imageTop) {
+        image[i].classList.add("show");
+      }
+    }
+  })
+});
+
+window.addEventListener('load',() => {
+  const image = document.querySelectorAll(".wrapper-image");
+  document.addEventListener("scroll",() => {
+    for (let i =0; i < image.length; i++) {
+      const imageTop = image[i].getBoundingClientRect().top + image[i].clientHeight * .6
+      if (window.innerHeight > imageTop) {
+        image[i].classList.add("show");
+      }
+    }
+  })
+});
+
+window.addEventListener('load',() => {
+  const loadButton = document.getElementById("reload")
+  loadButton.addEventListener('click',() => {
+    window.location.reload();
+    console.log("要素の確認")
+  })
+});
